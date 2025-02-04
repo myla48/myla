8aa076f95304bce797ddae693adf86aed4e29747
myla48 <ruhamazerihun2@gmail.com>
2025-02-04 20:59:03 +0300

    Create  Beautiful Matrix.cpp

---
+++ Beautiful Matrix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+    int a[5][5];  
+    int i, j, x, y;
+ 
+    
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            cin >> a[i][j];
+            if (a[i][j] == 1) {
+                x = i;
+                y = j;
+            }
+        }
+    }
+ 
+ 
+    cout << abs(x - 2) + abs(y - 2) << std::endl;
+ 
+    return 0;
+}
