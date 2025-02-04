3578e105f545c12e9440e65fb6056cb7e70c45ee
myla48 <ruhamazerihun2@gmail.com>
2025-02-04 21:01:39 +0300

    Create  Gravity Flip.cpp

---
+++ Gravity Flip.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+    int n; 
+    cin >> n;
+ 
+    int cubes[n];
+ 
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> cubes[i];
+    }
+ 
+   
+    sort(cubes, cubes + n);
+ 
+   
+    for (int i = 0; i < n; ++i) {
+        cout << cubes[i] << " ";
+    }
+    cout << endl;
+ 
+    return 0;
+}
